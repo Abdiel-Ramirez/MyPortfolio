b512bcbb2e056ae75f8ac3a4518e69e2a001e906
Abdiel-Ramirez <158487878+Abdiel-Ramirez@users.noreply.github.com>
2024-02-08 00:31:03 -0600

    Error is due to how github and next optimize images and routes

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -55,7 +55,7 @@ const About = () => {
       </div>
       <div className={styles.faceContain}>
         <Image
-          src="/MyPortafolio/Abby.png"
+          src="https://abdiel-ramirez.github.io/MyPortafolio/Abby.png"
           width={340}
           height={480}
           alt="Picture of the author"
