85afa0c540ef6eb9401ce4c37ce13efc906875ec
Abdiel-Ramirez <158487878+Abdiel-Ramirez@users.noreply.github.com>
2024-04-17 10:54:33 -0600

    Added Google Verification Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,12 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="https://abdiel-ramirez.github.io/MyPortafolio/apple-touch-icon.png?" />
         <link rel="icon" type="image/png" sizes="32x32" href="https://abdiel-ramirez.github.io/MyPortafolio/favicon-32x32.png?" />
         <link rel="icon" type="image/png" sizes="16x16" href="https://abdiel-ramirez.github.io/MyPortafolio/favicon-16x16.png?" />
-        <link rel="manifest" href="https://abdiel-ramirez.github.io/MyPortafolio/site.webmanifest?" />
         <link rel="mask-icon" href="https://abdiel-ramirez.github.io/MyPortafolio/safari-pinned-tab.svg?" color="#5bbad5" />
+        <link rel="manifest" href="https://abdiel-ramirez.github.io/MyPortafolio/site.webmanifest?" />
+        <meta name="google-site-verification" content="wsTkxnD3VpzO3b17NlvSMK94Gy6KEUCXXwohtS2mCf0" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        
 
       </head>
       <body className={inter.className}>{children}</body>
