d173004fdab594334aba249e734064ef73f0a327
Abdiel-Ramirez <158487878+Abdiel-Ramirez@users.noreply.github.com>
2024-08-07 11:36:38 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${poppins.variable}`}>
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="https://abdiel-ramirez.github.io/MyPortafolio/apple-touch-icon.png?" />
-        <link rel="icon" type="image/png" sizes="32x32" href="https://abdiel-ramirez.github.io/MyPortafolio/favicon-32x32.png?" />
-        <link rel="icon" type="image/png" sizes="16x16" href="https://abdiel-ramirez.github.io/MyPortafolio/favicon-16x16.png?" />
-        <link rel="mask-icon" href="https://abdiel-ramirez.github.io/MyPortafolio/safari-pinned-tab.svg?" color="#5bbad5" />
-        <link rel="manifest" href="https://abdiel-ramirez.github.io/MyPortafolio/site.webmanifest?" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://abdiel-ramirez.github.io/MyPortfolio/apple-touch-icon.png?" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://abdiel-ramirez.github.io/MyPortfolio/favicon-32x32.png?" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://abdiel-ramirez.github.io/MyPortfolio/favicon-16x16.png?" />
+        <link rel="mask-icon" href="https://abdiel-ramirez.github.io/MyPortfolio/safari-pinned-tab.svg?" color="#5bbad5" />
+        <link rel="manifest" href="https://abdiel-ramirez.github.io/MyPortfolio/site.webmanifest?" />
         <meta name="google-site-verification" content="wsTkxnD3VpzO3b17NlvSMK94Gy6KEUCXXwohtS2mCf0" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
