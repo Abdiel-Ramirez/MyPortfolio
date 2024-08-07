71df42f7b273a9aadcbb3a94474be7ce6f031d71
Abdiel-Ramirez <158487878+Abdiel-Ramirez@users.noreply.github.com>
2024-08-07 11:34:28 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output:'export',
-  basePath: '/MyPortafolio',
-  assetPrefix: '/MyPortafolio/',
+  basePath: '/MyPortfolio',
+  assetPrefix: '/MyPortfolio/',
   images: {
     unoptimized: true,
   },
